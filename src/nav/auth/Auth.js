ef32049b6d172f14f60821b5794eaa2d7ca457ed
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-03-30 13:49:33 -0700

    added padding to auth view

--- src/nav/auth/Auth.js
+++ src/nav/auth/Auth.js
@@ -65,7 +65,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingBottom: 40
   },  
   logo: {
     height: width / 2.5
